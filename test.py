bdbd03f1bc53555f929315ae5d75b35907371868
raiyanyahya <raiyanyahya@users.noreply.github.com>
2020-12-18 14:51:58 +0000

    [create-pull-request] automated change

--- test.py
+++ test.py
@@ -7,36 +7,43 @@ from cashman.model.transaction_type import TransactionType
 app = Flask(__name__)
 
 transactions = [
-  Income('Salary', 5000),
-  Income('Dividends', 200),
-  Expense('pizza', 50),
-  Expense('Rock Concert', 100)
+    Income("Salary", 5000),
+    Income("Dividends", 200),
+    Expense("pizza", 50),
+    Expense("Rock Concert", 100),
 ]
 
-@app.route('/incomes')
+
+@app.route("/incomes")
 def get_incomes():
-  schema = IncomeSchema(many=True)
-  incomes = schema.dump(filter(lambda t: t.type == TransactionType.INCOME, transactions))
-  return jsonify(incomes.data)
-@app.route('/incomes', methods=['POST'])
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
+    return jsonify(incomes.data)
+
+
+@app.route("/incomes", methods=["POST"])
 def add_income():
-  income = IncomeSchema().load(request.get_json())
-  transactions.append(income.data)
-  return "", 204
-@app.route('/expenses')
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income.data)
+    return "", 204
+
+
+@app.route("/expenses")
 def get_expenses():
-  schema = ExpenseSchema(many=True)
-  expenses = schema.dump(
-      filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
-  )
-  return jsonify(expenses.data)
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses.data)
 
 
-@app.route('/expenses', methods=['POST'])
+@app.route("/expenses", methods=["POST"])
 def add_expense():
-  expense = ExpenseSchema().load(request.get_json())
-  transactions.append(expense.data)
-  return "", 204
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense.data)
+    return "", 204
 
 
 if __name__ == "__main__":
